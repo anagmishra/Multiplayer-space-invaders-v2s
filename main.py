03b75b977badc2fcd6f22a27b5828362a4a11f65
anagmishra <anag.mishra@gmail.com>
2024-12-06 19:59:16 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+import pygame
+import os
+
+pygame.font.init()
+
+WIDTH = 900
+HEIGHT = 500
+
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("LET THE GAMES BEGIN")
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+VEL = 5
+
+BORDER = pygame.Rect(445, 0, 10, HEIGHT)
+
+HEALTH_FONT = pygame.font.SysFont('bauhaus93', 40)
+WINNER_FONT = pygame.font.SysFont('bauhaus93', 100)
+
+SPACESHIP_WIDTH = 55
+SPACESHIP_HEIGHT = 40
+
+YELLOW_HIT = pygame.USEREVENT + 1
+RED_HIT = pygame.USEREVENT + 2
+
+YELLOW_SPACESHIP_IMG = pygame.image.load(os.path.join('Pictures','yellow_spaceship.png'))
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMG, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+
+RED_SPACESHIP_IMG = pygame.image.load(os.path.join('Pictures', 'red_spaceship.png'))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMG, (SPACESHIP_WIDTH,SPACESHIP_HEIGHT)), 270)
+
+SPACE = pygame.transform.scale(pygame.image.load(os.path.join('Pictures', 'space.png')), (WIDTH, HEIGHT))
+
+def draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health):
+    WINDOW.blit(SPACE, (0, 0))
+    pygame.draw.rect(WINDOW, BLACK, BORDER)
+    red_health_text = HEALTH_FONT.render("Health: " + str(red_health), 1, WHITE)
+    yellow_health_text = HEALTH_FONT.render("Health: " + str(yellow_health), 1, WHITE)
+    WINDOW.blit(red_health_text, (WIDTH- red_health_text.get_width() - 10, 10))
+    WINDOW.blit(yellow_health_text, (10, 10))
+
+    WINDOW.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
+    WINDOW.blit(RED_SPACESHIP, (red.x, red.y))
+
+    for bullet in red_bullets:
+        pygame.draw.rect(WINDOW, RED, bullet)
+
+    for bullet in red_bullets:
+        pygame.draw.rect(WINDOW, YELLOW, bullet)
+
+    pygame.display.update()
+
+
+def yellow_handle_movement(keys_pressed, yellow):
+    if(keys_pressed[pygame.K_a] and yellow.x - VEL > 0):
+        yellow.x -= VEL
+    if(keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < BORDER.x):
+        yellow.x += VEL
